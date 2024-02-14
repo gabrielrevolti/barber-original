19e4ea35ff22a6a9d799c3b01821e92c621b814d
Gabriel Revolti <gabriel.revolti@gmail.com>
2024-02-14 09:38:33 -0300

    feat: add ServiceItem component

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -1,19 +1,25 @@
+"use client";
+
 import { Badge } from "@/app/_components/ui/badge";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Barbershop } from "@prisma/client";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface BarbershopItemProps {
   barbershop: Barbershop
 }
 
 const BarberShopItem = ({barbershop}: BarbershopItemProps) => {
-  barbershop
+  const router = useRouter()
+  const handleBookingClick = () => {
+    router.push(`/barbershops/${barbershop.id}`)
+  }
   return ( 
     <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
-      <CardContent className="px-1 py-0">
+      <CardContent className="px-1 py-0 pt-1">
         <div className="w-full h-[159px] relative">
           <div className="absolute top-2 left-2 z-50">
             <Badge variant="secondary" className="opacity-90 flex gap-1 items-center">
@@ -39,7 +45,9 @@ const BarberShopItem = ({barbershop}: BarbershopItemProps) => {
           <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap mt-2">
             {barbershop.address}
           </p>
-          <Button className="w-full mt-3" variant="secondary">Reservar</Button>
+          <Button className="w-full mt-3" variant="secondary" onClick={handleBookingClick}>
+            Reservar
+          </Button>
         </div>
       </CardContent>
     </Card>
