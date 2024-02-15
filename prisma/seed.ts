540dd0c964165c2c5dfc9b79748debf2e7c66aa5
Gabriel Revolti <gabriel.revolti@gmail.com>
2024-02-15 10:31:37 -0300

    fix: route and db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -120,7 +120,7 @@ async function seedDatabase() {
                 id: barbershop.id,
               },
             },
-            imageUrl: service.imageUrl
+            imageUrl: service.imageUrl,
           },
         });
       }
