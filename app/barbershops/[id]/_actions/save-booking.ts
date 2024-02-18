ab963ef8e5749162d91d883c6ce5a9722e3826df
Gabriel Revolti <gabriel.revolti@gmail.com>
2024-02-17 22:00:38 -0300

    feat: ensure a service can be booked by the user

---
+++ app/barbershops/[id]/_actions/save-booking.ts
@@ -0,0 +1,21 @@
+"use server"
+
+import { db } from "@/app/_lib/prisma";
+
+interface SaveBookingParams {
+  barbershopId: string;
+  serviceId: string;
+  userId: string
+  date: Date
+}
+
+export const SaveBooking = async (params: SaveBookingParams) => {
+  await db.booking.create({
+    data: {
+      serviceId: params.serviceId,
+      userId: params.userId,
+      date: params.date,
+      barbershopId: params.barbershopId
+    }
+  })
+}
