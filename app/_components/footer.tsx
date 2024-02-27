51a73b7108d376658b010e578d07c3e31ac588e1
Gabriel Revolti <gabriel.revolti@gmail.com>
2024-02-27 13:11:20 -0300

    feat: ensure semantic tags are used on footer and header

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,8 +1,10 @@
 const Footer = () => {
   return ( 
-    <div className="w-full bg-secondary py-6 px-5">
-      <p className="text-gray-400 text-xs font-bold opacity-75">© 2024 Copyright FSW Barber</p>
-    </div>
+    <footer>
+      <div className="w-full bg-secondary py-6 px-5">
+        <p className="text-gray-400 text-xs font-bold opacity-75">© 2024 Copyright FSW Barber</p>
+      </div>
+    </footer>
    );
 
 }
